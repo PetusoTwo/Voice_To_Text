26561f7a806b36f5ba450737801fc07aa18837aa
PetusoTwo <pedrocespedes456@gmail.com>
2024-08-13 13:01:14 -0500

    function principal

--- main.py
+++ main.py
@@ -7,23 +7,6 @@ import sys
 # Inicializar el reconocedor
 recognizer = sr.Recognizer()
 
-# Usar el micrófono para capturar el audio
-def convertText():
-    with sr.Microphone() as source:
-        print("Ajustando el micrófono. Por favor, espere...")
-        recognizer.adjust_for_ambient_noise(source)  # Ajustar el ruido ambiental
-        print("¡Listo para hablar!")
-        audio = recognizer.listen(source)
-
-    try:
-    # Usar Google Web Speech API para reconocer el audio
-        text = recognizer.recognize_google(audio, language= 'es- ES')
-        print("Texto reconocido: " + text)
-    except sr.UnknownValueError:
-        print("No se pudo entender el audio")
-    except sr.RequestError as e:
-        print("No se pudo conectar con el servicio de Google Speech Recognition; {0}".format(e))
-
 class MyApp(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyApp, self).__init__()
@@ -36,23 +19,36 @@ class MyApp(QtWidgets.QMainWindow):
         
         #Conexion a los botones
         self.close.clicked.connect(lambda: self.close())
-        #self.voice_txt.clicked.connect(convertText()) #Crear funcion#
+        self.voice_txt.clicked.connect(self.convertText)
         self.copytext.clicked.connect(self.clearData)
         self.copytext.clicked.connect(self.copyText)
         
     def clearData(self):
         self.output.setText("")
         self.output.clear()
+
     def copyText(self):
         #Condicionales para copiar tecxto#
         if (self.output is not None):
             QMessageBox.information(self, "Error", "No hay texto para copiar")
         else:
             QMessageBox.information(self, "Copiado", "Copiado al portapapeles")
-        
-        
-        
-        
+    def convertText(self):
+        with sr.Microphone() as source:
+            QMessageBox.information(self, "Micrófono", "Ajustando el micrófono. Por favor, espere...")
+            recognizer.adjust_for_ambient_noise(source)  # Ajustar el ruido ambiental
+            audio = recognizer.listen(source)
+            # Capturar el audio y posibles errores
+            try:
+                # Usar Google Web Speech API para reconocer el audio
+                text = recognizer.recognize_google(audio, language='es-ES')
+                self.output.setText(text)
+                QMessageBox.information(self, "Éxito", "Texto reconocido exitosamente")
+            except sr.UnknownValueError:
+                QMessageBox.critical(self, "Error", "No se pudo entender el audio")
+            except sr.RequestError as e:
+                QMessageBox.critical(self, "Error", f"No se pudo conectar con el servicio de Google Speech Recognition; {e}")
+
     #Funciones para que la ventana se pueda mover#
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
